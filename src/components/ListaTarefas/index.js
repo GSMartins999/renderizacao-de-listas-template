927174909b6de63c171f89739de1a9a442742e84
Giovanni Souza <GSMartins999>
2023-09-10 15:00:24 -0300

    renderizacaolistafeito

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -10,17 +10,37 @@ import {
 } from "./styled";
 import bin from "../../assets/bin.png";
 
+const listaDeTarefas = [];
 export function ListaTarefas() {
   const [novaTarefa, setNovaTarefa] = useState("");
+  const [lista, setLista] = useState(listaDeTarefas);
+
+  // const listaMapeada = lista.map((item) => {
+  //   return (
+  //     <Tarefa>
+  //       <p>Nova tarefa</p>
+  //       <RemoveButton>
+  //         <img src={bin} alt="" width="16px" />
+  //       </RemoveButton>
+  //     </Tarefa>
+  //   );
+  // });
 
 
   const onChangeTarefa = (event) => {
     setNovaTarefa(event.target.value);
   };
 
-  const adicionaTarefa = () => {};
+  const adicionaTarefa = () => {
+    const novaLista = [...lista, novaTarefa]
+    setLista(novaLista)
+    setNovaTarefa("")
+  };
 
-  const removeTarefa = () => {};
+  const removeTarefa = (valor) => {
+    const filtrarLista = lista.filter(item => item !== valor);
+    setLista(filtrarLista);
+  };
 
   return (
     <ListaTarefasContainer>
@@ -30,17 +50,29 @@ export function ListaTarefas() {
           value={novaTarefa}
           onChange={onChangeTarefa}
         />
-        <AddTaskButton>Adicionar</AddTaskButton>
+        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
       </InputContainer>
 
       <ListaContainer>
         <ul>
-          <Tarefa>
+          {/* <Tarefa>
             <p>Nova tarefa</p>
             <RemoveButton>
               <img src={bin} alt="" width="16px" />
             </RemoveButton>
-          </Tarefa>
+          </Tarefa> */}
+          {/* {listaMapeada} */}
+          {lista.map((item) => {
+              return (
+                <Tarefa key={item}>
+                  <p>Nova Tarefa</p>
+                  <RemoveButton onClick={() => removeTarefa(item)}>
+                    <img src={bin} alt="" width="16px" />
+                  </RemoveButton>
+                </Tarefa>
+              );
+            })
+          }
         </ul>
       </ListaContainer>
     </ListaTarefasContainer>
